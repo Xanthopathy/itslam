d48faac3978958edf50db4e4be645aff74cda3dd
Xanthopathy <xanthopathy0@gmail.com>
2026-07-03 16:05:52 +0700

    chore: replace adapter-auto with adapter-static in configuration files

---
+++ svelte.config.js
@@ -0,0 +1,18 @@
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+
+export default {
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      pages: "build",
+      assets: "build",
+      fallback: undefined,
+      precompress: false,
+      strict: true,
+    }),
+    paths: {
+      base: process.env.NODE_ENV === "production" ? "/your-repo-name" : "",
+    },
+  },
+};
