d0389601d4dcc6bfe5ad3993b89c536d5bc5f460
Xanthopathy <xanthopathy0@gmail.com>
2026-07-24 15:56:17 +0700

    feat: implement dynamic page title based on game status

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
   import "../styles/global.css";
   import favicon from "$lib/assets/favicon.svg";
+  import { gameEngine } from "$lib/gameStore.svelte";
 
   let { children } = $props();
+
+  const status = $derived(gameEngine.state.status);
+
+  const pageTitle = $derived.by(() => {
+    let title = "ITSLAM";
+    if (status === "lobby") title += " | In Lobby";
+    else if (status === "playing") title += " | In Game";
+    else if (status === "finished") title += " | Game Over";
+    return title;
+  });
 </script>
 
 <svelte:head>
+  <title>{pageTitle}</title>
   <link rel="icon" href={favicon} />
 </svelte:head>
 
